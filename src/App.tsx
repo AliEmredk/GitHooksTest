11beebc995a19261503edaa3b7c6e935a1c5d85f
AliEmredk <aliemreuzunoglu@gmail.com>
2025-10-23 13:46:57 +0200

    chore: test to fail

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-    const x: number = "hello"; // ❌
+    const x: number = "hello"; // ❌ dd
     console.log(x);
 
   return (
